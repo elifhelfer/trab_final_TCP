c70d117bceed64859f739191d9e182ca87aaec67
Guifernandes0521 <guifernandes0521@gmail.com>
2024-12-11 20:55:39 -0300

    changed the test file to receive multiple tracks in the sequencer

--- TrabalhoFinalTCP/src/main/java/TESTE.java
+++ TrabalhoFinalTCP/src/main/java/TESTE.java
@@ -9,9 +9,12 @@ public class TESTE {
 
         //System.out.println("Enter the input text to play (e.g., R+ABCDEFG BPM+): ");
         //String inputText = in.nextLine();
-        String inputText = "aR+aR-aBPM+aR+aR-aBPM+aR+aR-aR+aR-a";
-        MusicParser parser = new MusicParser();
+        String inputText = "o?oBPM+abcR+abcR+abcR-;abcBPM+abcoiu?";
+        String inputText2 = "";
+        MusicParser parser = new MusicParser(1);
+        MusicParser parser2 = new MusicParser(2);
         ArrayList<MidiEvent> midiEvents = parser.listMidiEvents(inputText);
+        ArrayList<MidiEvent> midiEvents2 = parser2.listMidiEvents(inputText2);
 
         try {
             // Get a Sequencer instance and open it
@@ -21,10 +24,13 @@ public class TESTE {
             // Create a new Sequence with Pulse Per Tick (PPQ) set to 4
             Sequence sequence = new Sequence(Sequence.PPQ, 4);
             Track track = sequence.createTrack();
-
+            Track track2 = sequence.createTrack();
             // Add parsed MIDI events to the track
             for (MidiEvent event : midiEvents) {
                 track.add(event);
+            }
+            for (MidiEvent event : midiEvents2) {
+                track2.add(event);
             }
 
             // Set the BPM of the sequencer

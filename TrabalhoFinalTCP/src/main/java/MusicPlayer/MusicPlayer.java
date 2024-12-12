f64ccfc8fd52218d5151321f35d17dbc11019f16
unknown <larifurtadohelfer@gmail.com>
2024-12-12 00:05:56 -0300

    simple music player, gotta see what else needed. maybe implement option to stop playing midway (interface)

--- TrabalhoFinalTCP/src/main/java/MusicPlayer/MusicPlayer.java
+++ TrabalhoFinalTCP/src/main/java/MusicPlayer/MusicPlayer.java
@@ -1,29 +1,62 @@
 package MusicPlayer;
 
+import MusicMaker.MusicParser;
+
 import javax.sound.midi.*;
 import java.util.*;
 
+import static java.lang.Thread.sleep;
+
 public class MusicPlayer {
-    private ArrayList<Track> tracks = null;
-    private Sequencer sequence = null;
 
-    public MusicPlayer(){
-        this.sequence = new Sequence(Sequence.PPQ, 4);
+    public static void PlayMusic(Sequence sequence){
+        try{
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
 
-        for (Track track : tracks){
-            this.sequence.
+            // Set the BPM of the sequencer
+            sequencer.setTempoInBPM(120);
+
+            // Assign the sequence to the sequencer and start it
+            sequencer.setSequence(sequence);
+            sequencer.start();
+
+            while (sequencer.isRunning()) {
+                sleep(1000);
+            }
+            sequencer.stop();
         }
-
+        catch(Exception ex){
+            //to be added
+        }
     }
 
-    private void addTrack(ArrayList<MidiEvent> song){
-        Track track = null;
-        for (MidiEvent event : song){
-            track.add(event);
+    public static void main(String[] args){
+        String inputText = "ao?oBPM+abcR+abcR+abcR-;abcBPM+abcoiu?";
+        String inputText2 = "";
+        MusicParser parser = new MusicParser(1,160);
+        MusicParser parser2 = new MusicParser(2,60);
+        ArrayList<MidiEvent> midiEvents = parser.listMidiEvents(inputText);
+        ArrayList<MidiEvent> midiEvents2 = parser2.listMidiEvents(inputText2);
+
+        try{
+            Sequence sequence = new Sequence(Sequence.PPQ, 4);
+            Track track = sequence.createTrack();
+            Track track2 = sequence.createTrack();
+            // Add parsed MIDI events to the track
+            for (MidiEvent event : midiEvents) {
+                track.add(event);
+            }
+            for (MidiEvent event : midiEvents2) {
+                track2.add(event);
+            }
+
+            MusicPlayer.PlayMusic(sequence);
+
+        }catch(Exception ex){
+            //nothing here yet
         }
-        this.tracks.add(track);
     }
-
-
-
 }
+
+
